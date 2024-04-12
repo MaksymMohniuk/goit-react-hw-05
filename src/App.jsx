9fb312ab5e5cf5d333404736f36cd8db471dc141
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-04-13 00:36:22 +0300

    added-NavLinks-&-Routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-// import { useState } from 'react'
+import { NavLink, Routes, Rout } from "react-router-dom";
 
-import "./App.css";
+import HomePage from "./pages/HomePage";
+import MoviesPage from "./pages/HomePage";
+
+import clsx from "clsx";
+import styles from "./App.module.css";
+
+const getNavLinkClassName = ({ isActive }) =>
+  clsx(styles.navLink, { [styles.active]: isActive });
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <header>
+        <nav className={styles.nav}>
+          <NavLink className={getNavLinkClassName} to="/">
+            Home
+          </NavLink>
+          <NavLink className={getNavLinkClassName} to="/movies-page">
+            Movies
+          </NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          <Rout path="/" element={<HomePage />}></Rout>
+          <Rout path="/movies-page" element={<MoviesPage />}></Rout>
+        </Routes>
+      </main>
+    </>
+  );
 }
 
 export default App;
