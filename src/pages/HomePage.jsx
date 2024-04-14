a40ec52014294d9be286701aceb96649035e4501
MaksymMohniuk <112825661+MaksymMohniuk@users.noreply.github.com>
2024-04-15 00:29:42 +0300

    deployed-homepage&loader

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,38 @@
+import { useState, useEffect } from "react";
+import { getTrendingMovies } from "../services/api";
+import Loader from "../components/Loader/Loader";
+
 const HomePage = () => {
-  return <div></div>;
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchTrendingMovies = async () => {
+      try {
+        setIsLoading(true);
+        const movies = await getTrendingMovies();
+        setTrendingMovies(movies);
+      } catch (error) {
+        console.error("Error fetching trending movies:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchTrendingMovies();
+  }, []);
+
+  return (
+    <div>
+      <h1>Trending today</h1>
+      {isLoading && <Loader />}
+      <ul>
+        {trendingMovies.map((movie) => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default HomePage;
---
+++ src/services/api.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+
+axios.defaults.baseURL = "https://api.themoviedb.org/3";
+axios.defaults.headers = {
+  Authorization:
+    "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NTRkNjNiNmQzZTY2MDhkMmQwZmU5NTI1YzcwNDg1MSIsInN1YiI6IjY2MTQ0ZjQwMTVhNGExMDE0YWY3YWNlMCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.FtWYqkMYGBjfgRUqpg07TWD8c6NIQFo-dNpccsLFvPc",
+};
+axios.defaults.params = { language: "en-US" };
+
+export const getTrendingMovies = async () => {
+  const { data } = await axios.get("trending/movie/day");
+  return data.results;
+};
+
+export const getMoviesByQuery = async (query) => {
+  const { data } = await axios.get(`/search/movie?query=${query}`);
+  return data.results;
+};
+
+export const getMovieDetails = async (movie_id) => {
+  const { data } = await axios.get(`movie/${movie_id}`);
+  return data.results;
+};
+
+export const getMovieCredits = async (movie_id) => {
+  const { data } = await axios.get(`movie/${movie_id}/credits`);
+  return data.results;
+};
+
+export const getMovieReviews = async (movie_id) => {
+  const { data } = await axios.get(`movie/${movie_id}/reviews`);
+  return data.results;
+};
